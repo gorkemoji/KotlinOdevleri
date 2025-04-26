84e48ce86a6ad25dc5e04df4e253c3b22ba8df7b
Görkem <emircan9921@outlook.com>
2025-04-26 23:38:46 +0300

    Create Exercise.kt

---
+++ Exercise.kt
@@ -0,0 +1,99 @@
+class Exercise {
+    val sehir = "İstanbul"
+    val ulke = "Türkiye"
+    val telefon = "5391234567"
+    val postaKodu = "34000"
+    val email = "gorkem@gmail.com"
+    val meslek = "Bilgisayar Mühendisliği"
+    val stokMiktari = 10
+    val musteriAdi = "Görkem Ece"
+    val bakiye = 1000
+    val dogumGunu = "20-01-2000"
+    val maas = 22000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Memnun kaldım."
+    val odemeTarihi = "20-02-2025"
+    val odeme = 100
+    val siparisAdeti = 10
+    val arabaModeli = "Opel Astra"
+    val kitapAdi = "Sefiller"
+    val yayinlamaTarihi = 2000
+    val indirimMiktari = 15
+    val odaSayisi = 3
+    val enlem = 39.0020
+    val boylam = 42.0082
+    val urunAdi = "iPhone 14"
+    val yemekFiyati = 750
+    val marka = "Apple"
+    val muzikAdi = "Sen Ağlama"
+    val videoSuresi = 360 // saniye
+    val urunPuani = 4.7
+    val resimAdi = "IMF_0010.jpg"
+    val dosyaFormati = "jpeg"
+    val renk = "Mavi"
+    val renkKodu = "#FFFFFF"
+    val telefonModeli = "iPhone 14"
+    val ekranBoyutu = 6.1
+    val agirlik = 120
+    val ulusalGun = "29-10"
+    val tatilGunu = "01-01"
+    val rezervasyonTarihi = "05-04-2025"
+    val sokakAdi = "İzmiryolu Caddesi"
+    val otobusHatti = "130Ş"
+    val kalanDakika = 10
+    val takipKodu = "TR123456789"
+    val kuponSuresi = "60 gün"
+    val kuponKodu = "INDIRIM100"
+    val faturaAdresi = "Moda, Kadıköy, İstanbul"
+}
+
+fun main() {
+    val exercise = Exercise()
+
+    println("Şehir: ${exercise.sehir}")
+    println("Ülke: ${exercise.ulke}")
+    println("Telefon: ${exercise.telefon}")
+    println("Posta Kodu: ${exercise.postaKodu}")
+    println("Email: ${exercise.email}")
+    println("Meslek: ${exercise.meslek}")
+    println("Stok Miktarı: ${exercise.stokMiktari}")
+    println("Müşteri Adı: ${exercise.musteriAdi}")
+    println("Bakiye: ${exercise.bakiye}")
+    println("Doğum Günü: ${exercise.dogumGunu}")
+    println("Maaş: ${exercise.maas}")
+    println("Medeni Durum: ${exercise.medeniDurum}")
+    println("Ürün Yorumu: ${exercise.urunYorum}")
+    println("Ödeme Tarihi: ${exercise.odemeTarihi}")
+    println("Ödeme: ${exercise.odeme}")
+    println("Sipariş Adeti: ${exercise.siparisAdeti}")
+    println("Araba Modeli: ${exercise.arabaModeli}")
+    println("Kitap Adı: ${exercise.kitapAdi}")
+    println("Yayınlama Tarihi: ${exercise.yayinlamaTarihi}")
+    println("İndirim Miktarı: ${exercise.indirimMiktari}")
+    println("Oda Sayısı: ${exercise.odaSayisi}")
+    println("Enlem: ${exercise.enlem}")
+    println("Boylam: ${exercise.boylam}")
+    println("Ürün Adı: ${exercise.urunAdi}")
+    println("Yemek Fiyatı: ${exercise.yemekFiyati}")
+    println("Marka: ${exercise.marka}")
+    println("Müzik Adı: ${exercise.muzikAdi}")
+    println("Video Süresi: ${exercise.videoSuresi} saniye")
+    println("Ürün Puanı: ${exercise.urunPuani}")
+    println("Resim Adı: ${exercise.resimAdi}")
+    println("Dosya Formatı: ${exercise.dosyaFormati}")
+    println("Renk: ${exercise.renk}")
+    println("Renk Kodu: ${exercise.renkKodu}")
+    println("Telefon Modeli: ${exercise.telefonModeli}")
+    println("Ekran Boyutu: ${exercise.ekranBoyutu} inç")
+    println("Ağırlık: ${exercise.agirlik} gram")
+    println("Ulusal Gün: ${exercise.ulusalGun}")
+    println("Tatil Günü: ${exercise.tatilGunu}")
+    println("Rezervasyon Tarihi: ${exercise.rezervasyonTarihi}")
+    println("Sokak Adı: ${exercise.sokakAdi}")
+    println("Otobüs Hattı: ${exercise.otobusHatti}")
+    println("Kalan Dakika: ${exercise.kalanDakika}")
+    println("Takip Kodu: ${exercise.takipKodu}")
+    println("Kupon Süresi: ${exercise.kuponSuresi}")
+    println("Kupon Kodu: ${exercise.kuponKodu}")
+    println("Fatura Adresi: ${exercise.faturaAdresi}")
+}
